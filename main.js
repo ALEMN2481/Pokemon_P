5c36dd770f135bc2f008f7b9c611f7af35d31873
Juan Sebastian Ruiz <juansebasrr0106@gmail.com>
2023-05-11 18:22:24 -0500

    JS

---
+++ main.js
@@ -0,0 +1,8 @@
+const listaPokemon = document.querySelector("#listaPokemon");
+let URL ="https://pokeapi.co/api/v2/pokemon/";
+
+for (let i =1; 1<= 151; i++){
+
+    fetch(URL + i)
+        .then((respone) => Response.json)
+}
