757c2c720e61c5936a67df0f39979a35613f667d
Juan Sebastian Ruiz <juansebasrr0106@gmail.com>
2023-05-11 18:39:50 -0500

    ERROR JS

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const listaPokemon = document.querySelector("#listaPokemon");
 let URL ="https://pokeapi.co/api/v2/pokemon/";
 
-for (let i =1; 1<= 151; i++){
+for (let i =1; 1<= 151; i++) {
 
     fetch(URL + i)
         .then((respone) => Response.json)
+        .then(data => console.log(data))
+}
+
+function mostrarPokemon(data){
+
+
 }
