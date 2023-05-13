c93faabee9013a7c6cf2e4db03e4d583ffcbeabc
Juan Sebastian Ruiz <juansebasrr0106@gmail.com>
2023-05-12 19:12:09 -0500

    Terminando JS

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 const listaPokemon = document.querySelector("#listaPokemon");
 let URL ="https://pokeapi.co/api/v2/pokemon/";
 
-for (let i =1; 1<= 151; i++) {
-
+for (let i =1; i <= 151; i++) {
+ 
     fetch(URL + i)
-        .then((respone) => Response.json)
-        .then(data => console.log(data))
+        .then((respone) => respone.json())
+        .then(data => mostrarPokemon(data))
 }
 
-function mostrarPokemon(data){
-
+function mostrarPokemon(poke) {
+    
+ const div = document.createElement("div");
+ div.classList.add("pokemon")
+ div.innerHTML = `<div class="pokemon">
+ <p class="pokemon-id-back">#${poke.id}</p>
+   <div class="pokemon-imagen">
+    <img src="${poke.sprites.other.home.front_default}"alt="${poke.name}">
+   </div>
+   <div class="pokemon-info">
+      <div class="nombre-contenedor">
+          <p class="pokemon-id">#${poke.id}</p>
+          <h2 class="pokemon-nombre">${poke.name}</h2>
+      </div>
+      <div class="pokemon-tipos">
+          <p class="electric tipo">ELECTRIC</p>
+          <p class="fighting tipo">FIGHTING</p>
+      </div>
+      <div class="pokemon-stats">
+          <p class="stat">${poke.height}m</p>
+          <p class="stat">${poke.weight}k</p>
+      </div>
+   </div>
+</div> 
+`;
+listaPokemon.append(div);
 
 }
